ef2c94fe70900260bf4bc75f328bc6c8b14c25f2
cristinecula <VirusZ@gmail.com>
2024-05-02 09:19:12 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-dropdown@10c3437e893fedb6101a57ac071e31c8fd957c8a 🚀

--- inline-module-a412f23235eb971c4a27c3950e98fd49.js
+++ inline-module-a412f23235eb971c4a27c3950e98fd49.js
@@ -11,7 +11,7 @@ if (SERVER_CHANNEL_URL) {
 }
 
 const importers = {
-  "./stories/cosmoz-dropdown.stories.ts": () => import('./cosmoz-dropdown.stories-tulLLAzk.js')
+  "./stories/cosmoz-dropdown.stories.ts": () => import('./cosmoz-dropdown.stories-0X1osYVe.js')
 };
 function importFn(path) {
   return importers[path]();
